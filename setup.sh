57f5f6b13e01e61573d256bbab57c74d1aa33d29
ni-lie <williejr@twelvebaskets.com.ph>
2025-03-13 08:41:17 +0800

    feat: add Rill installation

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 # Install DuckDB CLI
 curl https://install.duckdb.org | sh
 
-# Install Python dependencies
+# Install dbt-core with duckdb adapter
 python -m pip install dbt-core dbt-duckdb
 
-
+# Install Rill for creating dashboard
+curl https://rill.sh | sh
